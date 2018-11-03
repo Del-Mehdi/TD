76c8fbcb452f343cba582b00bfc7d1ec13017a17
Hmimnat <hmimnatelmehdi@hotmail.com>
2018-11-03 18:08:17 -0400

    threading sloved

--- client.py
+++ client.py
@@ -2,10 +2,9 @@ import socket
 
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect(('localhost',1064))
+socket.connect(('localhost',1001))
 print ("Connection on {}".format('1064'))
-for i in range(300):
-    socket.send(u"AAAA\n")
+socket.send("transaction")
 
 
 socket.close()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-class Server:
+class Server():
     def __init__(self,ip,port,name,cnx=1):
         self.ip = ip
         self.port = port 
@@ -11,18 +11,33 @@ class Server:
         self.name  = name
         print 'The {} is  Listening on {}:{}'.format(self.name , self.ip, self.port)
 
-    def run(self):
+    def listen(self):
         while True:
-            self.sock.listen(self.cnx)
-            self.client, self.address = self.sock.accept()
-            print 'The {} is  connected on {}:{}'.format(self.address[0] , self.ip, self.port)
-            self.message = self.client.recv(1024)
-            print(self.message.upper())
+            try:
+                self.sock.listen(self.cnx)
+                self.client, self.address = self.sock.accept()
+                print 'The {} is  connected on {}:{}'.format(self.address[0] , self.ip, self.port)
+                self.message = self.client.recv(8192)
+            except:
+                print("error in openning connection on {}  on port {}".format(self.ip,self.port))
 
+
+    def run(self):
+        self.t = threading.Thread(target=self.listen,name=self.name)
+        self.t.start()
+    #connect to the other server 
+    def connect(self,ipsrv,portsrv):
+        try:
+            self.con  = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            self.con.connect((ipsrv,portsrv))
+            print("connected to {} on port {}".format(ipsrv,portsrv))
+        except:
+            print ("error in connection to server")
 
     def close(self):
         self.sock.close()
 
-srvtest = Server('127.0.0.1',1064,"server1",10)
+srvtest = Server('127.0.0.1',1005,"server1",10)
 srvtest.run()
+srvtest.connect('127.0.0.1',2001)
 srvtest.close()
